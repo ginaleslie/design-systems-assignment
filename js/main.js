64ad7cf422baeabb364e1441c96f1381b5cf9d0d
Gina <ginal@indiefin.com>
2020-09-03 12:15:20 +0200

    Updated branch

--- js/main.js
+++ js/main.js
@@ -14,4 +14,25 @@ $(function () {
 		localStorage.setItem("userName", name);
 		window.location.href = "./pages/signed-up.html";
 	});
+	/* jQuery
+================================================== */
+	$(function () {
+		$(".acc__title").click(function (j) {
+			var dropDown = $(this).closest(".acc__card").find(".acc__panel");
+			$(this).closest(".acc").find(".acc__panel").not(dropDown).slideUp();
+
+			if ($(this).hasClass("active")) {
+				$(this).removeClass("active");
+			} else {
+				$(this)
+					.closest(".acc")
+					.find(".acc__title.active")
+					.removeClass("active");
+				$(this).addClass("active");
+			}
+
+			dropDown.stop(false, true).slideToggle();
+			j.preventDefault();
+		});
+	});
 });
