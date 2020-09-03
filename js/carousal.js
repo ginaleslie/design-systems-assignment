e9a06c4a4420e95a40f8e5d5f7116a8b6c50f530
Gina <ginal@indiefin.com>
2020-09-03 11:49:56 +0200

    Main js added

--- js/carousal.js
+++ js/carousal.js
@@ -63,6 +63,36 @@ $(function () {
 					'<a href="#" class="cover"><img src="../images/Mask Group (1).png" class="sliderImg" /><h6>Lorem ipsum dolor sit amet consectetur</h6><p class="grey-one">Samantha Smith<span class="grey-two float-r">Free</span></p></a>',
 			},
 		],
+		topFree: [
+			{
+				course:
+					'<a href="#" class="cover"><img src="../images/Mask Group (1) copy 4.png" class="sliderImg" /><h6>Lorem ipsum dolor sit amet consectetur</h6><p class="grey-one">Samantha Smith<span class="grey-two float-r">Free</span></p></a>',
+			},
+			{
+				course:
+					'<a href="#" class="cover"><img src="../images/Mask Group (1) copy.png" class="sliderImg" /><h6>Lorem ipsum dolor sit amet consectetur</h6><p class="grey-one">Samantha Smith<span class="grey-two float-r">Free</span></p></a>',
+			},
+			{
+				course:
+					'<a href="#" class="cover"><img src="../images/Mask Group (2).png" class="sliderImg" /><h6>Lorem ipsum dolor sit amet consectetur</h6><p class="grey-one">Samantha Smith<span class="grey-two float-r">Free</span></p></a>',
+			},
+			{
+				course:
+					'<a href="#" class="cover"><img src="../images/Component 4.png" class="sliderImg" /><h6>Lorem ipsum dolor sit amet consectetur</h6><p class="grey-one">Samantha Smith<span class="grey-two float-r">Free</span></p></a>',
+			},
+			{
+				course:
+					'<a href="#" class="cover"><img src="../images/Mask Group (1) copy 2.png" class="sliderImg" /><h6>Lorem ipsum dolor sit amet consectetur</h6><p class="grey-one">Samantha Smith<span class="grey-two float-r">Free</span></p></a>',
+			},
+			{
+				course:
+					'<a href="#" class="cover"><img src="../images/Mask Group (1) copy 3.png" class="sliderImg" /><h6>Lorem ipsum dolor sit amet consectetur</h6><p class="grey-one">Samantha Smith<span class="grey-two float-r">Free</span></p></a>',
+			},
+			{
+				course:
+					'<a href="#" class="cover"><img src="../images/Mask Group (1).png" class="sliderImg" /><h6>Lorem ipsum dolor sit amet consectetur</h6><p class="grey-one">Samantha Smith<span class="grey-two float-r">Free</span></p></a>',
+			},
+		],
 	});
 	$("#carousalData").append(carousalItems);
 
